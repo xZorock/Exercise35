6c25ea5a4e9c45a7b6efe5b328e37fe2cedab0a3
Jason Di Bartolo <jasondibartolo98@gmail.com>
2023-03-21 20:00:09 +0100

    Second Commit

--- src/it/develhope/exercise35/Start.java
+++ src/it/develhope/exercise35/Start.java
@@ -5,7 +5,7 @@ public class Start {
 
         String s1 = "are you able to climb, are you able to swim or are you able to fly?";
 
-        String s2 = s1.replaceAll("(\s+are)","_XYZ");
+        String s2 = s1.replaceAll("(\s*+are)","_XYZ");
 
         System.out.println(s2);
     }
